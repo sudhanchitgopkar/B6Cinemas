231d258a82a956eaae6cc04ce53988ca4cb12f76
Sudhan Chitgopkar <sudhan@osx.local>
2022-09-30 09:31:20 -0400

    added: register, login, confirmation, forgot pwd UI

---
+++ B6Website/registerCollapsible.js
@@ -0,0 +1,13 @@
+var coll = document.getElementsByClassName("optional");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    } //if
+  });
+} //for
