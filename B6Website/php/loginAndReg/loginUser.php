90c2c906b2a68179de9a41559da7f5ccbf8e801e
Catalyst-01 <mattbutler072500@gmail.com>
2022-10-26 11:23:10 -0400

    added check for user active on login

--- B6Website/php/loginAndReg/loginUser.php
+++ B6Website/php/loginAndReg/loginUser.php
@@ -9,15 +9,18 @@
     $login_pass = filter_input(INPUT_POST, 'login_password');
 
     try {
-        $query = 'SELECT email, password_, type_, user_id FROM user WHERE email=:_email';
+        $query = 'SELECT email, password_, status_, type_, user_id FROM user WHERE email=:_email';
 
         $statement = $db->prepare($query);
         $statement->bindValue(':_email', $login_user);
         $statement->execute();
         $info = $statement->fetch();
 
-        if(password_verify($login_pass, $info['password_'])) {
-            // TODO start session with session variables for current logged in user HERE!!
+        // if user is inactive, don't allow login
+        if($info['status_'] == 1) {
+            echo '<script>alert("You must activate your account before you can login.")</script>';
+        } else if(password_verify($login_pass, $info['password_'])) {
+            // TODO start session with session variables for current logged in user
             $_SESSION['loggedin'] = true;
             $_SESSION['userID'] = $info['user_id'];
             $_SESSION['userType'] = $info['type_'];
@@ -30,6 +33,7 @@
 
         } else {
             // Notify User their username/password was incorrect
+            echo '<script>alert("Your password is incorrect.")</script>';
             header("Location: ../../view/loginAndReg/login.php");
         }
 
